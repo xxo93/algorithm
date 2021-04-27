592b44a36ff0ddfc2cfee6e98d1a3f063c383ac0
xxo93 <61908699+xxo93@users.noreply.github.com>
2021-04-27 11:51:55 +0800

    Create common.py

---
+++ 二叉树/common.py
@@ -0,0 +1,24 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+def list_to_tree(arr: list):
+    """ 列表结构转换成数结构 """
+
+    return
+
+
+def tree_to_2Dlist(treeNode):
+    """ 列表结构转换成数结构 """
+
+    return []
+
+
+
+
+if __name__ == '__main__':
+    root = TreeNode(1, 2, 3)
