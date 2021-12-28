c5cc569819426f11eecdebb84e40f1daaa49f20b
xxo93 <61908699+xxo93@users.noreply.github.com>
2021-12-28 10:33:24 +0800

    Update common.py

--- 链表/common.py
+++ 链表/common.py
@@ -4,6 +4,29 @@ class ListNode:
         self.next = next
 
 
+def list_to_linklist(node_list: list) -> ListNode:
+    """ 给定列表，生成链表 """
+    if len(node_list) == 0:
+        return ListNode()
+    header = ListNode(node_list[0])
+    p = header
+    for node_val in node_list[1:]:
+        nxt = ListNode(node_val)
+        p.next = nxt
+        p = p.next
+    return header
+
+
+def linklist_to_list(header: ListNode) -> list:
+    """ 给定链表，生成列表 """
+    node_val_list = []
+    p = header
+    while p:
+        node_val_list.append(p.val)
+        p = p.next
+    return node_val_list
+
+
 def get_linked_list(v_lis: list) -> ListNode:
     """ 数组转链表 """
     lk_list = []
